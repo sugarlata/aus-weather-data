9200ed3d0e5290e4397f71984bd2043d837a788c
Nathan Sgarlata <nathan.sgarlata@gmail.com>
2023-12-22 06:06:48 +1100

    Adds logging

---
+++ aus_weather_data/core/logger.py
@@ -0,0 +1,38 @@
+import logging
+import functools
+
+
+LOG_FORMAT = "%(asctime)s:%(name)s:%(funcName)s:%(message)s"
+GLOBAL_LOG_LEVEL = logging.DEBUG
+GLOBAL_LOG_FILE = "aus_weather_data.log"
+GLOBAL_LOG_STREAM = True
+
+
+def log(logger):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            def FUNCTION_CALL(func_name, args, kwargs):
+                logger.debug(f"CALL   {func_name}")
+                logger.debug(f"ARGS   {args}")
+                logger.debug(f"KWARGS {kwargs}")
+
+            def FUNCTION_EXIT(func_name, func_result):
+                logger.debug(f"END    {func_name}")
+                logger.debug(f"RESULT {func_result}")
+
+            args_repr = [repr(a) for a in args]
+            kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]
+            FUNCTION_CALL(func.__name__, args_repr, kwargs_repr)
+
+            try:
+                result = func(*args, **kwargs)
+                FUNCTION_EXIT(func.__name__, result)
+                return result
+            except Exception as e:
+                logger.exception(f"Exception raised in {func.__name__}")
+                raise e
+
+        return wrapper
+
+    return decorator
